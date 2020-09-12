1988f652adc248de28e12540a08aa207ab4b2984
albshayer <bedourb02@gmail.com>
2020-09-12 23:44:48 +0300

    تم

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+let family = ["ahmed", "bashayer", "bedour","bader"]
+var evens =  [2, 4, 6, 8, 10, 12]
+var grades = [89.7, 90.2, 80.1, 87,5]
+var trueFalse = "true, true, true, fales"
+
+print(family[0], family[3])
+print(evens[0], evens[5])
+print(grades[0], grades[3])
+
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,8 @@
+var fruits = ["mango", "orange", "potato", "peach"]
+print(fruits[0], fruits[3])
+fruits.append("strwberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["banana", "watermelon", "pineapple", "kiwi"]
+print(fruits)
